d6fa977b4cb80147e778eae0559686178a6b6e13
Muhammed Adhil p <167611139+adhilbathali@users.noreply.github.com>
2025-09-22 19:33:31 +0530

    add escape quote char

--- src/app/about-us/quote.js
+++ src/app/about-us/quote.js
@@ -1,10 +1,9 @@
-export default function Quote(){
-    return(
-        <div className="bg-blue-50 py-30 px-6 text-center">
-  <p className="text-xl md:text-2xl text-gray-800 italic">
-    "The greatest wealth is health — and our mission is to make it accessible to every life we touch."
-  </p>
-</div>
-
-    )
+export default function Quote() {
+  return (
+    <div className="bg-blue-50 py-30 px-6 text-center">
+      <p className="text-xl md:text-2xl text-gray-800 italic">
+        &quot;The greatest wealth is health — and our mission is to make it accessible to every life we touch.&quot;
+      </p>
+    </div>
+  );
 }
