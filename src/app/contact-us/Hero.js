5a3e559351ec0ba76e7e274dfa90ec3bcf3e9d93
Muhammed Adhil p <167611139+adhilbathali@users.noreply.github.com>
2025-09-23 16:14:04 +0530

    edit about us

--- src/app/contact-us/Hero.js
+++ src/app/contact-us/Hero.js
@@ -17,14 +17,14 @@ export default function ContactHero() {
       <div className="absolute inset-0 bg-black/50 z-10"></div>
 
       {/* Content */}
-      <div className="relative z-20 max-w-4xl mx-auto px-4 text-center text-white">
+      <div className="relative z-20 max-w-4xl mx-auto px-4 text-center text-white max-md:pt-20">
         <h1 className="text-4xl sm:text-5xl font-bold mb-4">Get in Touch</h1>
         <p className="text-lg sm:text-xl mb-12">
           Have a question about our products or services, or want to discuss a potential collaboration? Reach out and our team will respond promptly.
         </p>
 
         {/* Contact Info */}
-        <div className="grid sm:grid-cols-3 gap-8">
+        <div className="grid min-lg:grid-cols-3 max-md:grid-cols-1 max-md:grid-rows-3 gap-8">
           <div className="bg-white/20 backdrop-blur-md p-6 rounded-lg shadow hover:shadow-lg transition">
             <h2 className="text-lg font-semibold mb-2 text-blue-100">Email</h2>
             <p>xeltonpharma@gmail.com</p>
