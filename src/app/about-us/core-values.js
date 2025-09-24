81f0c1c6b5425a0ebac71d31c284d01f8c55d238
Muhammed Adhil p <167611139+adhilbathali@users.noreply.github.com>
2025-09-24 11:05:34 +0530

    contact us: edit core values

--- src/app/about-us/core-values.js
+++ src/app/about-us/core-values.js
@@ -7,7 +7,7 @@ export default function CoreValues(){
                 <div className="flex flex-col gap-10 px-5">
                     <h2 className="text-2xl font-bold mb-2 text-slate-700 text-center">Our Values</h2>
                     {corePillars.map((pillar, index) => (
-                        <article key={index} className="flex items-center gap-5 p-5 text-slate-700 text-lg max-md:rounded-2xl min-md:rounded-full bg-white min-w-1/2 transition-all duration-400 hover:scale-[1.02]">
+                        <article key={index} className="flex items-center gap-5 max-md:p-4 hyphens-auto min-md:p-5 text-slate-700 text-lg max-md:rounded-2xl min-md:rounded-full bg-white min-w-1/2 transition-all duration-400 hover:scale-[1.02]">
                             <div className="bg-blue-50 p-5 rounded-full">
                                 <FontAwesomeIcon icon={pillar.icon} className="w-10 h-10 text-blue-600 rounded-full"/>
                             </div>
