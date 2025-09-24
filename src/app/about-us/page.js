bd143184b6adf8b65e108927ecb45acd5bf0d96d
Muhammed Adhil p <167611139+adhilbathali@users.noreply.github.com>
2025-09-24 10:59:50 +0530

    contact-us: reduce gap for hero contents

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -18,13 +18,13 @@ export default function Hero() {
           {/* Overlay */}
           <div className="absolute inset-0 bg-black/40"></div>
           {/* Content */}
-          <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4 gap-15 mt-25">
-            <h1 className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
+          <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4 max-md:gap-10 min-md:gap-15 mt-25">
+            <h1 className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold">
               Building a Healthier India
             </h1>
             <div className="bg-white/30 backdrop-blur-md lg:w-1/2 flex p-10 rounded-xl">
                 <p className="text-lg md:text-xl text-white">
-                    Xelton Pharma was founded with the vision to revolutionize healthcare in India. Based in Mumbai, we are dedicated to bringing innovative and high-quality pharmaceutical products to the Indian market. Though new, our team is driven by experience, expertise, and a passion for improving lives.
+                    <b>Xelton Pharma</b> was founded with the vision to revolutionize healthcare in India. Based in Mumbai, we are dedicated to bringing innovative and high-quality pharmaceutical products to the Indian market. Though new, our team is driven by experience, expertise, and a passion for improving lives.
                 </p>
             </div>
 
